ae674e4bac4202365f76bbadfb52ec5de45655d2
sankethmesta <55999209+sankethmesta@users.noreply.github.com>
2021-10-24 07:19:27 -0700

    Add files via upload

---
+++ day 7 task1/test.js
@@ -0,0 +1,43 @@
+// addition
+var one=1;
+var two=2;
+var sum=one+two;
+console.log("sum is: "+sum);
+// subtraction
+var one=1;
+var two=2;
+var diff=one-two;
+console.log("diff is: "+diff);
+// multiplication
+var one=1;
+var two=2;
+var multi=one*two;
+console.log("multiplication is: "+multi);
+// mod
+var one=1;
+var two=2;
+var mod=one%two;
+console.log("mod is:" +mod);
+// division
+var one=1;
+var two=2;
+var div=one/two;
+console.log("divide is: "+div);
+// concatination
+var one="Aris";
+var two="Global";
+console.log("concatination of the string is: "+one+" "+two);
+// concatination using +=
+var one="Aris";
+var two="Global";
+one+= two;
+console.log("concatination: "+one)
+// increment
+var one=1;
+one++;
+console.log("increment: "+one);
+// decrement
+var one=1;
+one--;
+console.log("Decrement: "+one);
+
